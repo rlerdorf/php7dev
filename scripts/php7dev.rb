fb0953684de89c52c1926e0a7de54686bc559119
Svpernova09 <joe@joeferguson.me>
2015-01-30 13:32:58 -0600

    Add option to enable public DHCP network interface.

--- scripts/php7dev.rb
+++ scripts/php7dev.rb
@@ -7,6 +7,10 @@ class Php7dev
     # Configure A Private Network IP
     config.vm.network :private_network, ip: settings["ip"] ||= "192.168.7.7"
 
+    if settings['networking'][0]['public']
+      config.vm.network "public_network", type: "dhcp"
+    end
+
     # Configure A Few VirtualBox Settings
     config.vm.provider "virtualbox" do |vb|
       vb.name = 'php7dev'
