e024732bfc8abf029066ee9589a63bfab3f4e495
Svpernova09 <joe@joeferguson.me>
2015-01-30 08:53:51 -0600

    Fix path to php-fpm.sock

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -25,7 +25,7 @@ block="server {
 
     location ~ \.php$ {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/var/run/php5-fpm.sock;
+        fastcgi_pass unix:/var/run/php-fpm.sock;
         fastcgi_index index.php;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
